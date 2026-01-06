517c1463683f635ce358e89d85e2cb1ebfa1dc36
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2026-01-07 01:05:39 +0530

    build.ynmal

--- src/main/java/com/rakeshgupta/cafebrew_backend/CafebrewBackendApplication.java
+++ src/main/java/com/rakeshgupta/cafebrew_backend/CafebrewBackendApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-//rakesh.gupta.laitusneo@gmail.comProfile image
-rakesh.gupta.laitusneo@gmail.com
+// rakesh.gupta.laitusneo@gmail.com
 @SpringBootApplication
 @EnableScheduling
 public class CafebrewBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CafebrewBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CafebrewBackendApplication.class, args);
+    }
 }
